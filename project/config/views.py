0cef0ae3860daa12354afd5cf91d5d678a8533f1
facundojgonzalez <mambona2020@gmail.com>
2024-01-19 09:39:39 -0300

    Agrega Proyecto Django

--- project/config/views.py
+++ project/config/views.py
@@ -1,2 +1,8 @@
-def saludo (request):
-    return HttpResponse ("Saludo")
+from django.http import HttpResponse
+
+def saludo(request):
+    return HttpResponse("¡Hola!")
+
+def saludo2(request):
+    nombre = input('Ingresa tu nombre')
+    return HttpResponse(f'Tu nombre es {nombre}')
